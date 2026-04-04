a0fae10764042cc1551593545da02c571abee9b0
Ari <aril.abcmail@gmail.com>
2026-04-04 17:48:48 +0200

    probar

---
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+if [ ! -f /etc/ssl/certs/nginx.crt ]; then
+echo "Nginx: setting up ssl ...";
+mkdir -p /etc/nginx/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=wordpress/CN=abaldelo.42.fr";
+echo "Nginx: ssl is set up!";
+fi
+exec "$@"
